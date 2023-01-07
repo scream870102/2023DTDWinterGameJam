b58329bc23bf289d3fc474657067d5d21861a3b6
Scream <scream870102@gmail.com>
2023-01-08 02:15:54 +0800

    Effect import

--- Assets/Scripts/Sonar/Sonar.cs
+++ Assets/Scripts/Sonar/Sonar.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 using Scream.UniMO;
 using Scream.UniMO.Common;
 using System;
+using BIGJ2023.GameSystem;
+using Scream.UniMO.Utils;
 
 public class OnPlayerTrigger : IDomainEvent
 {
-    public Sonar.SonarState State{get; private set;}
-    public GameObject Player{get; private set;}
+    public Sonar.SonarState State { get; private set; }
+    public GameObject Player { get; private set; }
 
     public OnPlayerTrigger(Sonar.SonarState state, GameObject player)
     {
@@ -20,120 +22,116 @@ public class OnPlayerTrigger : IDomainEvent
 public class Sonar : MonoBehaviour
 {
     // Start is called before the first frame update
-    public enum Player{
-        player1, player2
+    public enum Player
+    {
+        player1,
+        player2
     }
 
-    public enum SonarState{
-        direct, indirect
+    public enum SonarState
+    {
+        direct,
+        indirect
     }
-    [SerializeField]private GameObject sonarPrefab;
-    [SerializeField]private float sonarMinRadius = 0.5f;
-    [SerializeField]private float sonarMaxRadius = 3;
-    [SerializeField]private float sonarDetectSpeed = 0.1f;
-    [SerializeField]private float sonarStayDuration = 0.5f;
-    [SerializeField]private float sonarMaskDetectSpeed = 0.5f;
+    [SerializeField] private float sonarMinRadius = 0.5f;
+    [SerializeField] private float sonarMaxRadius = 3;
+    [SerializeField] private float sonarStayDuration = 0.5f;
+    [SerializeField] private float sonarMaskDetectSpeed = 0.5f;
+    [SerializeField] private LayerMask detectLayer;
 
     private GameObject sonar;
     private CircleCollider2D sonarCollider;
-    private bool isSonarOpen = false;
-    public bool IsSonarOpen => isSonarOpen;
+    public bool IsSonarOpen { get; private set; }
 
-    private void Awake() {
-        DomainEvents.Register<OnPlayerAction>(onPlayerActionEvent);
+    private Coroutine playingSonar;
+    private GameObject parent;
+
+    private void Awake()
+    {
+        sonarCollider = GetComponent<CircleCollider2D>();
+        DomainEvents.Register<OnPlayerAction>(OnPlayerActionEvent);
     }
 
-    private void onPlayerActionEvent(OnPlayerAction obj)
+    private void OnDestroy()
     {
-        if(obj.ActionState == PlayerAction.detect && obj.player == this.gameObject)
+        DomainEvents.UnRegister<OnPlayerAction>(OnPlayerActionEvent);
+    }
+
+    private void OnPlayerActionEvent(OnPlayerAction param)
+    {
+        if (param.ActionState == PlayerAction.detect && param.player == parent)
         {
             Detect();
         }
-        else if(obj.ActionState == PlayerAction.counter && obj.player == this.gameObject)
+        else if (param.ActionState == PlayerAction.counter && param.player == parent)
         {
             Counter();
         }
     }
 
-    private void Start()
+    private void OnPlayerTriggerEvent(OnPlayerTrigger param) { }
+
+    public void Init(GameObject parent)
     {
-        InitSonar();
-    }
-
-    private void OnDestroy() {
-        //DomainEvents.UnRegister<OnPlayerAction>(onPlayerActionEvent);    
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
-    }
-
-    private void OnPlayerTriggerEvent(OnPlayerTrigger param){ }
-
-    
-
-    private void InitSonar()
-    {
-        sonar = Instantiate(sonarPrefab, this.transform);
-        sonarCollider = this.gameObject.AddComponent<CircleCollider2D>();
-        SpriteMask sonarMask = sonar.GetComponent<SpriteMask>();
-        sonarCollider.isTrigger = true;
+        this.parent = parent;
+        sonar = gameObject;
+        SetSonarOpen(false);
     }
 
     private void Detect()
     {
-        if(sonarCollider==null) return;
-        if(isSonarOpen) return;
-        // -1cd
-        StartCoroutine(SonarSpread());
-        return; 
+        if (sonarCollider == null) return;
+        if (IsSonarOpen) return;
+        //TODO: -1cd
+        playingSonar = MonoHelper.Instance.StartCoroutine(SonarSpread());
     }
 
     private void Counter()
     {
         Debug.Log("Try counter");
-        for(int i=0;i<PlayerManager.Instance.Players.Length;i++)
+
+        foreach (GameObject otherPlayerObj in PlayerManager.Instance.Players)
         {
-            if(PlayerManager.Instance.Players[i] != gameObject)
+            if (otherPlayerObj != parent)
             {
-                Sonar enemySonar = PlayerManager.Instance.Players[i].GetComponent<Sonar>();
-                if(enemySonar.isSonarOpen)
+                Sonar enemySonar = otherPlayerObj.GetComponent<Sonar>();
+                if (enemySonar.IsSonarOpen)
                 {
                     enemySonar.Reset();
                 }
             }
-            else{
+            else
+            {
                 Debug.Log("No one is using sonar???");
             }
         }
-        // -2(?)xcd
+        //TODO: -2(?)xcd
     }
-    private void OnTriggerEnter2D(Collider2D other) {
-        if(!isSonarOpen) return;
-        if(other.gameObject.tag == "Player")
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!IsSonarOpen) return;
+        if (other.gameObject.tag == ProjectConst.PlayerTag)
         {
-            
-            if(Physics2D.Linecast(PlayerManager.Instance.Players[0].transform.position,PlayerManager.Instance.Players[1].transform.position))
+
+            if (Physics2D.Linecast(PlayerManager.Instance.Players[0].transform.position, PlayerManager.Instance.Players[1].transform.position, detectLayer))
             {
-                DomainEvents.Raise(new OnPlayerTrigger(Sonar.SonarState.indirect,this.gameObject));
+                DomainEvents.Raise(new OnPlayerTrigger(Sonar.SonarState.indirect, parent));
                 Debug.Log("Indirect Collide player!!");
             }
             else
             {
                 Debug.Log("Direct Collide player!!");
-                DomainEvents.Raise(new OnPlayerTrigger(SonarState.direct,this.gameObject));
-            }            
+                DomainEvents.Raise(new OnPlayerTrigger(SonarState.direct, parent));
+            }
         }
     }
 
-    
-    private void OnTriggerStay2D(Collider2D other)    
+    private void OnTriggerStay2D(Collider2D other)
     {
-        if(!isSonarOpen) return;
-        if(other.gameObject.TryGetComponent<Sonar>(out Sonar collideSonar))
+        if (!IsSonarOpen) return;
+        if (other.gameObject.TryGetComponent<Sonar>(out Sonar collideSonar))
         {
-            if(collideSonar.IsSonarOpen)
+            if (collideSonar.IsSonarOpen)
             {
                 CancelAllSonar(collideSonar);
             }
@@ -142,33 +140,45 @@ public class Sonar : MonoBehaviour
 
     private void CancelAllSonar(Sonar collideSonar)
     {
-        this.Reset();
+        Reset();
         collideSonar.Reset();
     }
 
-    private void Reset() {
-        StopAllCoroutines();
-        isSonarOpen = false;
+    private void Reset()
+    {
+        if (playingSonar != null)
+        {
+            MonoHelper.Instance.StopCoroutine(playingSonar);
+            playingSonar = null;
+        }
+        SetSonarOpen(false);
         sonarCollider.radius = sonarMinRadius;
-        sonar.transform.localScale = new Vector3(1,1,1);
-
+        sonar.transform.localScale = Vector3.one;
     }
 
     IEnumerator SonarSpread()
     {
-        isSonarOpen = true;
-        while(sonarCollider.radius < sonarMaxRadius)
+        SetSonarOpen(true);
+        //while (sonarCollider.radius < sonarMaxRadius)
+        while (sonar.transform.localScale.x < sonarMaxRadius)
         {
-            sonarCollider.radius += sonarDetectSpeed;
-            sonar.transform.localScale += new Vector3(sonarMaskDetectSpeed,sonarMaskDetectSpeed,1);
-            yield return new WaitForSeconds(0.1f);
+            //sonarCollider.radius += sonarDetectSpeed * Time.deltaTime;
+            sonar.transform.localScale += new Vector3(sonarMaskDetectSpeed * Time.deltaTime, sonarMaskDetectSpeed * Time.deltaTime, 1);
+            yield return new WaitForSeconds(Time.deltaTime);
         }
         yield return new WaitForSeconds(sonarStayDuration);
         Reset();
     }
 
-    private void OnDrawGizmos() {
-        if(sonarCollider != null)
+    private void SetSonarOpen(bool isOpen)
+    {
+        IsSonarOpen = isOpen;
+        gameObject.SetActive(isOpen);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (sonarCollider != null)
         {
             Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(transform.position, sonarCollider.radius);
