f9efe6fa1c4c3e4c5d903b99f607613e485202a2
Accelerator87 <n126970@gmail.com>
2023-01-08 16:59:53 +0800

    Add Player Animations Scripts

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,19 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Scream.UniMO.Common;
+using Scream.UniMO.Utils;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
     public float MoveSpeed, StunnedTime;
-    private bool StunnedCheck = false;
+    public bool StunnedCheck = false;
     public KeyCode upKeyCode, downKeyCode, leftKeyCode, rightKeyCode;
 
     void Start() {
-        
+        StunnedTime = 4f;
     }
 
     void Update() {
+        transform.gameObject.GetComponent<Animator>().SetBool("Shift", false);
         if(!StunnedCheck) {
             // detect direction
             float up, down, left, right;
@@ -21,7 +24,10 @@ public class PlayerMove : MonoBehaviour
             down  = Input.GetKey(downKeyCode)  ? -1f : 0f;
             left  = Input.GetKey(leftKeyCode)  ? -1f : 0f;
             right = Input.GetKey(rightKeyCode) ? +1f : 0f;
-
+            
+            if((up == 1f) || (down == -1f) || (left == -1f) || (right == 1f)) {
+                transform.gameObject.GetComponent<Animator>().SetBool("Shift", true);
+            }
             // movement
             Vector3 movement = new Vector3((left+right)*MoveSpeed*Time.deltaTime, (up+down)*MoveSpeed*Time.deltaTime, 0);
             transform.Translate(movement);
@@ -37,6 +43,7 @@ public class PlayerMove : MonoBehaviour
     private void OnPlayerTriggerEvent(OnPlayerTrigger param) {
         if (param.State == Sonar.SonarState.indirect) return;
         if (param.Player == transform.gameObject) return;
+        transform.gameObject.GetComponent<Animator>().SetBool("Stunned", true);
         transform.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
         StunnedCheck = true;
         StartCoroutine(OnStunnedEnd());
@@ -45,6 +52,7 @@ public class PlayerMove : MonoBehaviour
         yield return new WaitForSeconds(StunnedTime);
         transform.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
         StunnedCheck = false;
+        transform.gameObject.GetComponent<Animator>().SetBool("Stunned", false);
     }
     private void OnDestroy() {
         DomainEvents.UnRegister<OnPlayerTrigger>(OnPlayerTriggerEvent);
