c17a13bc8d6d2427082b266436e00de75deb9986
Scream <scream870102@gmail.com>
2023-01-08 15:20:35 +0800

    追加UI Logic

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -3,25 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using Scream.UniMO.Common;
 
-
-
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance;
-    [SerializeField]private GameObject[] players;
-    public GameObject[] Players=>players;
-    private void Awake() {
-        
-        if(Instance == null)
+    [SerializeField] private GameObject[] players;
+    public GameObject[] Players => players;
+    private void Awake()
+    {
+
+        if (Instance == null)
         {
             Instance = this;
         }
-        else{
+        else
+        {
             Destroy(this);
         }
     }
 
-    private void OnDrawGizmos() {
-        Gizmos.DrawLine(players[0].transform.position,players[1].transform.position);
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(players[0].transform.position, players[1].transform.position);
+    }
+
+    public int GetPlayerIndex(GameObject playerObj)
+    {
+        for (int i = 0; i < players.Length; i++)
+        {
+            if (players[i] == playerObj)
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 }
